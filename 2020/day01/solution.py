367f97efea125f28cbd992b7b5dc352955d40cc6
MetaBytez <will@wcj.dev>
2020-12-03 00:17:32 -0600

    Day 03 - Python

--- 2020/day01/solution.py
+++ 2020/day01/solution.py
@@ -1,8 +1,6 @@
 # https://adventofcode.com/2020/day/1
-from utils.decorators import timed
 
 
-@timed
 def part_1() -> int:
     """633216"""
     with open('input.txt') as f:
@@ -20,19 +18,6 @@ def part_1() -> int:
             return target*num
 
 
-@timed
-def part_1_old() -> int:
-    """633216"""
-    with open('input.txt') as f:
-        nums = list(map(int, f.readlines()))
-
-    for i, x in enumerate(nums):
-        for j, y in enumerate(nums[i+1:]):
-            if (x + y) == 2020:
-                return x*y
-
-
-@timed
 def part_2() -> int:
     """68348924"""
     with open('input.txt') as f:
@@ -49,19 +34,6 @@ def part_2() -> int:
             num_3 = remainder - num_2
             if num_3 in nums and num_3 != num_1:
                 return num_1*num_2*num_3
-
-
-@timed
-def part_2_old() -> int:
-    """68348924"""
-    with open('input.txt') as f:
-        nums = list(map(int, f.readlines()))
-
-    for i, x in enumerate(nums):
-        for j, y in enumerate(nums[i+1:]):
-            for k, z in enumerate(nums[j+1:]):
-                if (x + y + z) == 2020:
-                    return x*y*z
 
 
 if __name__ == '__main__':
---
+++ 2020/day03/solution.py
@@ -0,0 +1,49 @@
+# https://adventofcode.com/2020/day/3
+
+from typing import List, Tuple
+
+
+def count_trees(slope: Tuple[int, int], tree_map: List[str]) -> int:
+    x_slope, y_slope = slope
+    num_rows = len(tree_map)
+    row_len = len(tree_map[0])
+    tree_count = 0
+
+    for stops in range(1, (num_rows//y_slope)):
+        x, y = x_slope*stops, y_slope*stops
+        x = x % row_len
+        if tree_map[y][x] == '#':
+            tree_count += 1
+    return tree_count
+
+
+def part_1():
+    """250"""
+    with open('input.txt') as f:
+        tree_map = list(map(lambda x: x.strip(), f.readlines()))
+    return count_trees((3,1), tree_map)
+
+
+def part_2():
+    """1592662500"""
+    total = 1
+    slopes = [
+        (1,1),
+        (3,1),
+        (5,1),
+        (7,1),
+        (1,2),
+    ]
+
+    with open('input.txt') as f:
+        tree_map = list(map(lambda x: x.strip(), f.readlines()))
+
+    for slope in slopes:
+        total *= count_trees(slope, tree_map)
+
+    return total
+
+
+if __name__ == '__main__':
+    print(part_1())
+    print(part_2())
